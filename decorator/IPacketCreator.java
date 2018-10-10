4bbd2a45c5e989b86859794fcaf6c50c2979131b
lishanyun <lishanyun@smates.cn>
2018-10-10 10:14:53 +0800

    装饰者模式

--- decorator/IPacketCreator.java
+++ decorator/IPacketCreator.java
@@ -1,5 +1,9 @@
 package design_mode.decorator;
-
+/**
+ * 组件接口
+ * @author lsy
+ * 2018年10月10日上午10:13:24
+ */
 public interface IPacketCreator {
 	public String handleContent();// 用于内容处理
 }
--- decorator/PacketBodyCreator.java
+++ decorator/PacketBodyCreator.java
@@ -1,5 +1,9 @@
 package design_mode.decorator;
-
+/**
+ * 具体组件（被装饰的对象）
+ * @author lsy
+ * 2018年10月10日上午10:14:07
+ */
 public class PacketBodyCreator implements IPacketCreator {
 
 	@Override
